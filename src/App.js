aac73b446fa447918ac46fef265b3ac06137903a
srinivas0210 <bhsrinivas.2000@gmail.com>
2021-09-21 02:03:38 +0530

    fix:resolved suspense issue

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserProfileList />
     </div>
   );
 }
+
+
+const fetchUserProfile = (userId) => {
+  let status = "pending"
+  let result;
+
+  let promise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({ name: `name-${userId}`, email: `email-${userId}` });
+    }, 1000);
+  });
+
+  let suspender = promise.then(
+    (r) => {
+      status = "success";
+      result = r;
+    },
+    (e) => {
+      status = "error";
+      result = e;
+    }
+  );
+
+  return {
+    read() {
+      if (status === "pending") {
+        throw suspender;
+      } else if (status === "error") {
+        throw result;
+      } else if (status === "success") {
+        return result;
+      }
+    }
+  };
+};
+
+
+const SuspensefulUserProfile = ({ userId }) => {
+  let resource = fetchUserProfile(userId);
+
+  return (
+    <Suspense fallback={<div> loading...</div>}>
+      <UserProfile resource={resource} />
+    </Suspense>
+  );
+};
+
+const UserProfile = ({ resource }) => {
+  const data = resource.read();
+  return (
+    <>
+      <h1>{data.name}</h1>
+      <h2>{data.email}</h2>
+    </>
+  );
+};
+
+const UserProfileList = () => {
+  return (
+    <>
+      <SuspensefulUserProfile userId={1} />
+      <SuspensefulUserProfile userId={2} />
+      <SuspensefulUserProfile userId={3} />
+    </>
+  );
+};
+
 
 export default App;
